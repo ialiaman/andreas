590384e1b6a629dcb00c43098d0aa70db9a4c423
ialiaman <iamanhunzai@gmail.com>
2022-04-26 02:00:30 +0500

    Payment integration

--- client/src/Pages/Dashboard/Setting.js
+++ client/src/Pages/Dashboard/Setting.js
@@ -436,8 +436,30 @@ const Overview = () => {
     </div>
   );
 };
+const ProductDisplay = () => <section></section>;
+const Message = ({ message }) => (
+  <section>
+    <p>{message}</p>
+  </section>
+);
 const Subscriptions = () => {
+  const [message, setMessage] = useState("");
   const [freePrice, setFreePrice] = useState(0);
+
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+
+    if (query.get("success")) {
+      setMessage("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      setMessage(
+        "Order canceled -- continue to shop around and checkout when you're ready."
+      );
+    }
+  }, []);
 
   useEffect(() => {
     axios
@@ -454,6 +476,7 @@ const Subscriptions = () => {
   };
   return (
     <div className="container">
+      {message ? <Message message={message} /> : <ProductDisplay />}
       <h3>Subscriptions</h3>
       <p>Choose your plan</p>
       <div className="row">
@@ -474,7 +497,12 @@ const Subscriptions = () => {
                 <span style={price}>Free</span>
                 <span>8 days</span>
               </div>
-              <Button title="Subscribe Now" type="primaryFullWidth" />
+              <form
+                action="http://localhost:3001/create-checkout-session1"
+                method="POST"
+              >
+                <Button title="Subscribe Now" type="primaryFullWidth" />
+              </form>
               <br />
               <br />
               <p>Track up to 50 leads</p>
@@ -484,25 +512,6 @@ const Subscriptions = () => {
             </div>
           </div>
           <br />
-          <div className="card">
-            <div className="card-body">
-              <h1>
-                <b>Addons</b>
-              </h1>
-              <p>Best for learning and chatting with customers.</p>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                }}
-              >
-                <span style={price}>10$</span>
-                <span>Per lead</span>
-              </div>
-              <Button title="Add Now" type="primaryFullWidth" />
-            </div>
-          </div>
         </div>
         <div className="col-12 col-md-4">
           <div className="card">
@@ -521,7 +530,12 @@ const Subscriptions = () => {
                 <span style={price}>$16</span>
                 <span>Monthly</span>
               </div>
-              <Button title="Subscribe Now" type="primaryFullWidth" />
+              <form
+                action="http://localhost:3001/create-checkout-session1"
+                method="POST"
+              >
+                <Button title="Subscribe Now" type="primaryFullWidth" />
+              </form>
               <br />
               <br />
               <p>Track up to 200 Leads</p>
@@ -538,9 +552,9 @@ const Subscriptions = () => {
           <div className="card">
             <div className="card-body">
               <h1>
-                <b>Business</b>
+                <b>Addons</b>
               </h1>
-              <p>Key features, advanced reporting, and workflow automation.</p>
+              <p>Best for learning and chatting with customers.</p>
               <div
                 style={{
                   display: "flex",
@@ -548,20 +562,10 @@ const Subscriptions = () => {
                   alignItems: "center",
                 }}
               >
-                <span style={price}>$60</span>
-                <span>Monthly</span>
+                <span style={price}>10$</span>
+                <span>Per lead</span>
               </div>
-              <Button title="Subscribe Now" type="primaryFullWidth" />
-              <br />
-              <br />
-              <p>Track up to 5000 Leads</p>
-              <p>Unlimited chat history</p>
-              <p>Customization</p>
-              <p>Analytics</p>
-              <p>Software engineer support</p>
-              <p>Multiple website support</p>
-              <p>LiveChat Dashboard</p>
-              <p>White label chat widget</p>
+              <Button title="Add Now" type="primaryFullWidth" />
             </div>
           </div>
         </div>
